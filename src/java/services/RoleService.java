30633314840b5f56de773884d2e446fcce9d7789
user <user@10.0.0.176>
2022-10-19 15:10:19 -0600

    Finished services layer. will have to polish later.

--- src/java/services/RoleService.java
+++ src/java/services/RoleService.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package services;
+
+import dataaccess.RoleDB;
+import java.util.ArrayList;
+import models.Role;
 
 /**
  *
  * @author user
  */
 public class RoleService {
+    
+    public ArrayList<Role> getAll() throws Exception {
+        RoleDB roleDB = new RoleDB();
+        ArrayList<Role> roles = roleDB.getAll();
+        return roles;
+    }
     
 }
