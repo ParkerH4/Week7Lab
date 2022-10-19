7a5671372e63e5efe723d9e72b59cc8f09511dd3
user <user@10.0.0.176>
2022-10-19 13:43:00 -0600

    Models package completed. Role and Users classes done.

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models;
+
+import java.io.Serializable;
 
 /**
  *
  * @author user
  */
-public class Role {
+public class Role implements Serializable {
+   
+    private int roleID;
+    private String roleType;
+
+    public Role() {
+    }
+
+    public Role(int roleID, String roleType) {
+        this.roleID = roleID;
+        this.roleType = roleType;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+    
     
 }
